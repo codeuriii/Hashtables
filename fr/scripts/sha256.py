a1f2b3af7d524e30bf7003ba684d7b9d1f5a52e3
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2024-02-01 11:02:24 +0400

    Add sha256 script

---
+++ fr/scripts/sha256.py
@@ -0,0 +1,24 @@
+import hashlib
+import json
+
+def hash_md5(input_string: str):
+    # Convertir la chaîne en bytes
+    input_bytes = input_string.encode()
+    
+    # Créer un objet hasher MD5
+    md5_hasher = hashlib.sha256()
+    
+    # Mettre à jour le hasher avec les bytes de la chaîne
+    md5_hasher.update(input_bytes)
+    
+    # Récupérer le hash MD5 sous forme de chaîne hexadécimale
+    md5_hash = md5_hasher.hexdigest()
+    
+    return md5_hash
+data = {}
+with open("../dict-fr.txt", "r", encoding="utf-8") as f:
+    for line in f.readlines():
+        data[line.removesuffix("\n")] = hash_md5(line.removesuffix("\n"))
+    
+with open("../result/sha256.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
