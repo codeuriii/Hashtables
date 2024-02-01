4a65abdb530a77e28c96389d875a715452ac8eb0
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2024-02-01 11:06:48 +0400

    Add sha384

---
+++ fr/scripts/sha384.py
@@ -0,0 +1,20 @@
+import hashlib
+import json
+
+def hash_md5(input_string: str):
+    input_bytes = input_string.encode()
+    
+    md5_hasher = hashlib.sha384()
+    
+    md5_hasher.update(input_bytes)
+    
+    md5_hash = md5_hasher.hexdigest()
+    
+    return md5_hash
+data = {}
+with open("../dict-fr.txt", "r", encoding="utf-8") as f:
+    for line in f.readlines():
+        data[line.removesuffix("\n")] = hash_md5(line.removesuffix("\n"))
+    
+with open("../result/sha384.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
