16cc635164aa852e69c00ca0055df29ecc32afe4
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2024-02-01 12:43:25 +0400

    Add auto dump

--- all/gen.py
+++ all/gen.py
@@ -1,4 +1,5 @@
 from string import digits, ascii_letters, punctuation
+import json
 
 chars = list(digits + ascii_letters + punctuation)
 
@@ -9,3 +10,9 @@ def gen(reste: list[str]):
             data.append(f"{element}{char}")
     return data
 
+data = chars
+for i in range(1, 10):
+    with open(f"lists/{i}.json", "w") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    
+    data = gen(data)
